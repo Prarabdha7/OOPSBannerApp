8979c424f1e844e6b4b4e9528414ea990b8cb1ab
Prarabdha7 <prarabdhasachancr7n1@gmail.com>
2026-02-17 11:16:39 +0530

    Print OOPS Banner using String.join method

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,30 @@
+/**
+ * OOPSBannerApp UC3 – OOPS Banner Application (Use Case 3)
+ *
+ * This class extends the functionality of Use Case 2 by improving the way the "OOPS" banner
+ * is constructed and displayed. It focuses on enhancing code readability and efficiency
+ * by utilizing the String.join() method to create each line of the banner. This approach
+ * overcomes the drawbacks of using the + operator for string concatenation, which can lead
+ * to code inefficiency due to the creation of multiple intermediate String objects in memory.
+ *
+ * @author Prarabdha Sachan (RA2511003010562)
+ * @version 3.0
+ */
+// Extend use case UC2 to use String.join() method to create each line of the "OOPS" banner
+// instead of concatenating strings with the + operator. This is essential to overcome the
+// drawbacks of using the + operator for string concatenation which essentially creates
+// multiple intermediate String objects in memory leading to inefficiency.
+
+// Hint: Use String.join(" ", ...) to join the parts of each line with spaces.
+
+public class OOPSBannerApp {
+    public static void main(String[] args){
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " *****   ", "   ***   "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **  **  ", " **    **"));
+        System.out.println(String.join(" ", "**     **", "**     **", " **   ** ", " **      "));
+        System.out.println(String.join(" ", "**     **", "**     **", " ** **   ", "    **   "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **      ", "       **"));
+        System.out.println(String.join(" ", "**     **", "**     **", " **      ", " **    **"));
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " **      ", "   ***   "));
+        }
+}
